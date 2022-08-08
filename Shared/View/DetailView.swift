c267365eefbb2116bdce37273b5dcb1e3e0e470c
min woo <mlolw2@naver.com>
2022-08-08 20:49:37 +0900

    Step7

---
+++ Shared/View/DetailView.swift
@@ -0,0 +1,44 @@
+//
+//  DetailView.swift
+//  SwiftUImemo
+//
+//  Created by 전민우 on 2022/08/08.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    
+    @ObservedObject var memo : Memo
+    
+    @EnvironmentObject var store : MemoStore
+    
+    var body: some View {
+        VStack{
+            ScrollView{
+                VStack{
+                    HStack {
+                        Text(memo.content)
+                            .padding()
+                        
+                        Spacer(minLength: 0)
+                    }
+                    
+                    Text(memo.insertDate, style: .date)
+                        .padding()
+                        .font(.footnote)
+                        .foregroundColor(.secondary)
+                }
+            }
+        }
+        .navigationTitle("메모 보기")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView(memo: Memo(content: "Hello"))
+            .environmentObject(MemoStore())
+    }
+}
