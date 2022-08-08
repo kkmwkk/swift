4528c9a8517db772fb655a4bd6eec2f7871303a3
min woo <mlolw2@naver.com>
2022-08-08 19:53:17 +0900

    Step1

---
+++ Shared/MainListView.swift
@@ -0,0 +1,20 @@
+//
+//  MainListView.swift
+//  SwiftUImemo
+//
+//  Created by 전민우 on 2022/08/08.
+//
+
+import SwiftUI
+
+struct MainListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MainListView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainListView()
+    }
+}
